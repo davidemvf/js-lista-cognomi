465d1ae32490220bbb6c772bf2a5c8b51cb80ce4
davidemvf <davidemv.fiore@gmail.com>
2019-07-03 18:49:40 +0200

    Completato

---
+++ cognomi.js
@@ -0,0 +1,24 @@
+// Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+
+var utente = prompt("Cognome");
+var cognomi = ["Pisolo", "Gongolo", "Cucciolo", "Mammolo", "Brontolo", "Eolo", "Dotto"];
+
+
+// inserisco il cognome dell'utente all'interno dell'arrey
+var contenutotemp;
+var cognome = document.getElementById("mio_id")
+cognomi.push(utente);
+cognomi.sort();
+
+
+// Stampo a video la lista in ordine alfabetico
+
+for (var i = 0; i < cognomi.length; i++){
+contenutotemp = cognome.innerHTML;
+cognome.innerHTML = contenutotemp + "<li>" + cognomi[i] + "</li>";
+}
+
+
+//Stampo a video la nuova posizione del cognome dell'utente
+var position = cognomi.indexOf(utente) + 1;
+document.getElementById("posizione").innerHTML = position;
